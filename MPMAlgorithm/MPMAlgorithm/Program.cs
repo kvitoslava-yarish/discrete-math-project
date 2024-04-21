13e7dd878aaf17e2c8b70f5831bb593923489dc0
kvita <kyarish@kse.org.ua>
2024-04-21 21:39:31 +0300

    resolved conflicts

--- MPMAlgorithm/MPMAlgorithm/Program.cs
+++ MPMAlgorithm/MPMAlgorithm/Program.cs
@@ -1,13 +1,1 @@
-﻿using System;
-
-namespace MPMAlgorithm
-{
-    internal class Program
-    {
-        public static void Main(string[] args)
-        {
-            var graph1 = new MatrixGraph(20, 0.5, 20);
-            graph1.PrintGraph();
-        }
-    }
-}
+﻿
